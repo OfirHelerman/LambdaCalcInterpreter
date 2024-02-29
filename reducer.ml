d2c836979b44b361f30f7a20aa8f7ea1f8a4b826
OfirHelerman <ofirhelerman9@gmail.com>
2024-02-29 13:36:30 +0200

    works!

--- reducer.ml
+++ reducer.ml
@@ -21,13 +21,10 @@ let fresh_var used_vars : string =
 
 let rec fv = fun e ->
   match e with
-  | Variable x -> [x]
-  | Abstraction (x, t) -> List.filter (fun v -> v <> x) (fv t)
-  | Application (t1, t2) -> List.append (fv t1) (fv t2)
+  | Variable x -> StringSet.add x StringSet.empty 
+  | Abstraction (x, t) -> StringSet.remove x (fv t)
+  | Application (t1, t2) -> StringSet.union (fv t1) (fv t2)
 ;;
-(*
-  ADD FUNCTIONS BELOW
-*) 
 
 let extract_some = function
   | Some x -> x
@@ -36,31 +33,84 @@ let extract_some = function
 let rec substitute x s = fun t ->
   let free_vars_s = fv s in
   match t with
-  | Variable y when y = x -> s
-  | Variable y -> Variable y
-  | Abstraction(y, t1) when y = x -> Abstraction(x, t1)
-  | Abstraction(y, t1) when not (List.mem y free_vars_s) -> Abstraction(y, substitute x s t1)
-  | Abstraction(y, t1) -> (* here y <> x and y in FV(s) *) Abstraction("z", substitute x s (substitute y (Variable "z") t1))
+  | Variable y -> if y = x then s else Variable y 
+  | Abstraction(y, t1) -> 
+    if y = x then
+      Abstraction(x, t1)
+    else if (not (StringSet.mem y free_vars_s)) then
+      Abstraction(y, substitute x s t1)
+    (* here y <> x and y in FV(s) *)
+    else     
+      let free_vars_t1 = fv t1 in
+      let new_var = fresh_var (StringSet.union free_vars_t1 free_vars_s) in 
+      Abstraction(new_var, substitute x s (substitute y (Variable new_var) t1))
   | Application (t1, t2) -> Application (substitute x s t1, substitute x s t2)
 ;;
 
-let rec reduce_cbv = fun t ->
+(* let rec reduce_cbv = fun t ->
   match t with
   | Application (Abstraction (x, t1), t2) ->
-    let v = reduce_cbv t2 in (
-      match v with
-    | None -> Some (substitute x t2 t1)
-    | Some _ -> Some (Application (Abstraction (x, t1), substitute x (extract_some v) t1))
-    )
+    (match reduce_cbv t2 with
+    | Some t2' -> Some (substitute x t2' t1)
+    | None -> Some (substitute x t2 t1))
+  | Application (t1, t2) ->
+    (match reduce_cbv t2 with
+    | Some t2' -> Some (Application (t1, t2'))
+    | None -> 
+      match reduce_cbv t1 with
+      | Some t1' -> Some (Application (t1', t2))
+      | None -> None (* check if this is problematic*)
+      )
   | _ -> None
-  ;;
+;; *)
 
+(* let rec reduce_cbv = function
+(* base cases *)
+| Abstraction (_, _) -> None
+| Application (Abstraction(x, t1), Abstraction(y, t2)) ->
+  let v2 = Abstraction (y, t2) in
+  Some (substitute x v2 t1)
+(* recursive cases*)
+| Application (t1, t2) ->
+  (match reduce_cbv t1 with
+  | Some t1' -> Some (Application(t1', t2))
+  | None -> (* t1 is a value, abstraction*)
+    let v1 = t1 in
+    match reduce_cbv t2 with
+    | Some t2' -> Some (Application (v1, t2'))
+    | None -> None)
+| _ -> None
+;; *)
+
+let rec reduce_cbv = function
+  | Abstraction (_, _) -> None (* Base case: abstraction cannot be further reduced *)
+  | Application (Abstraction (x, t1), t2) ->
+    ( match reduce_cbv t2 with
+      | Some t2' -> Some (Application (Abstraction (x, t1), t2'))
+      | None -> 
+        (
+          match t2 with 
+          | Abstraction (_, _) -> Some (substitute x t2 t1)
+          | Variable _ -> Some (substitute x t2 t1)
+          | _ -> None
+        )
+    ) (* Reduction of application with abstraction *)
+  | Application (t1, t2) ->
+    (match reduce_cbv t1 with
+    | Some t1' -> Some (Application (t1', t2))
+    | None ->
+      (match reduce_cbv t2 with
+      | Some t2' -> Some (Application (t1, t2'))
+      | None -> None)) (* Non-abstraction application case *)
+  | _ -> None (* Base case: variables cannot be further reduced *)
+;;
 
 let rec reduce_cbn = fun t ->
-  match t with 
-  | Application (Abstraction (x, t1), t2) ->
-    let v = reduce_cbn t2 in
-    (match v with
-    | None -> Some (substitute x t2 t1)
-    | Some _ -> Some (Application (Abstraction (x, t1), (extract_some v))))
+  match t with
+  | Application (Abstraction (x, t1), t2) -> Some (substitute x t2 t1)
+  | Application (t1, t2) ->
+    (match reduce_cbn t1 with
+    | Some t1' -> Some (Application (t1', t2))
+    | None -> None) (* check if this is problematic*)
   | _ -> None
+;;
